9bd5dd0656ac49d7368f8dfb08d18e04ab91cab0
Nullable Z <stopLookingForMyEmail@teraproxy.memes>
2017-10-18 19:28:23 -0400

    added current information panel

--- teracli.js
+++ teracli.js
@@ -1,19 +1,25 @@
 var blessed = require('blessed');
+
 var screen = blessed.screen({
     smartCSR: true,
     autoPadding: true,
     dockBorders: true
 });
-
+screen.key(['escape', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
 screen.title = 'TERA Terminal Client';
-
-// Create a box perfectly centered horizontally and vertically.
-var box = blessed.listbar({
+setInterval(()=>{
+    screen.realloc();
+    screen.render();
+}, 1000);
+//Menu
+var menu = blessed.list({
     top: '0',
     left: '0',
-    width: '100%',
-    height: 'shrink',
-    items: ["Information", "Broker", "Chat"],
+    width: '20%',
+    height: '30%',
+    items: ["Home", "Trade Broker", "Chat", "Inventory", "Crafting"],
     tags: true,
     mouse: true,
     autoCommandKeys: true,
@@ -32,15 +38,33 @@ var box = blessed.listbar({
         }
     }
 });
-screen.append(box);
-
-// Quit on Escape or Control-C.
-screen.key(['escape', 'C-c'], function(ch, key) {
-  return process.exit(0);
+screen.append(menu);
+//info-panel
+var info = blessed.list({
+    top: '30%',
+    left: '0',
+    width: '20%',
+    height: '70%',
+    items: ["Current Information",
+            "Character Name:",
+            " > Kanawanai",
+            "Location:",
+            " > RK-9 Kennel"],
+    tags: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        selected:{
+            bold: true,
+            underline:true
+        },
+        fg: 'white',
+        bg: 'black',
+        border: {
+            fg: '#f0f0f0'
+        }
+    }
 });
-
-// Focus our element.
-box.focus();
-
-// Render the screen.
-screen.render();
+info.select(0);
+screen.append(info);
