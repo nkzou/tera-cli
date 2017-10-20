09fbf99dc9146060278dde12e7c62611b83be043
Nullable Z <stopLookingForMyEmail@teraproxy.memes>
2017-10-20 12:21:39 -0400

    merged headless client with blessed terminal UI; added functionality for incoming chat messages

--- index.js
+++ index.js
@@ -3,6 +3,148 @@ const config = require('./config/config.json')
 const servers = require('./config/servers.json')
 const webClient = require('tera-auth-ticket')
 const { Connection, FakeClient } = require('tera-proxy-game')
+var npmstring = require('string')
+var blessed = require('blessed')
+var screenshotmode = true
+var screen = blessed.screen({
+    smartCSR: true,
+    autoPadding: true,
+    dockBorders: true
+});
+screen.key(['escape', 'C-c'], function(ch, key) {
+    return process.exit(0);
+});
+screen.title = 'TERA Terminal Client';
+setInterval(()=>{
+    screen.realloc()
+    screen.render()
+}, 1000);
+//Menu
+var menu = blessed.list({
+    top: '0',
+    left: '0',
+    width: '20%',
+    height: '30%',
+    items: ["Home", "Chat", "Friend List", "Inventory", "Crafting", "Trade Broker", "Inspector"],
+    tags: true,
+    mouse: true,
+    autoCommandKeys: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        selected: {
+            fg: 'black',
+            bg: 'white'
+        },
+        fg: 'white',
+        bg: 'black',
+        border: {
+            fg: '#f0f0f0'
+        }
+    }
+});
+screen.append(menu);
+//info-panel
+var info = blessed.list({
+    top: '30%',
+    left: '0',
+    width: '20%',
+    height: '70%',
+    items: ["Current Information",
+            "Character Name:",
+            " > Redacted.Name",
+            "Location:",
+            " > Highwatch"],
+    tags: true,
+    border: {
+        type: 'line'
+    },
+    style: {
+        selected:{
+            underline:true
+        },
+        fg: 'white',
+        bg: 'black',
+        border: {
+            fg: '#f0f0f0'
+        }
+    }
+});
+info.select(0);
+screen.append(info);
+
+//contentbox
+var content = blessed.log({
+    content: "",
+    top: '0%',
+    left: '20%',
+    width: '80%',
+    height: '90%',
+    tags: true,
+    keys: true,
+    mouse: true,
+    border: {
+        type: 'line'
+    },
+    padding: {
+        bottom:0
+    },
+    style: {
+        fg: 'white',
+        bg: 'black',
+        border: {
+            fg: '#f0f0f0'
+        }
+    }
+});
+screen.append(content);
+
+var chat = blessed.textbox({
+    top: '90%',
+    left: '35%',
+    width: '65%',
+    height: 'shrink',
+    tags: true,
+    mouse: true,
+    inputOnFocus: true,
+    border: {
+        type: 'line'
+    },
+    padding: {
+        top:0
+    },
+    style: {
+        fg: 'white',
+        bg: 'black',
+        border: {
+            fg: '#f0f0f0'
+        }
+    }
+});
+var chatpanel = blessed.box({
+    content: "All Chat",
+    top: '90%',
+    left: '20%',
+    width: '15%',
+    height: 'shrink',
+    tags: true,
+    border: {
+        type: 'line'
+    },
+    padding: {
+        top:0
+    },
+    style: {
+        fg: 'white',
+        bg: 'black',
+        border: {
+            fg: '#f0f0f0'
+        }
+    }
+});
+screen.append(chatpanel);
+screen.append(chat);
 
 const describe = (() => {
 	const races = ['Human', 'High Elf', 'Aman', 'Castanic', 'Popori', 'Baraka']
@@ -25,10 +167,32 @@ const describe = (() => {
 			}
 		}
 
-		description += ' ' + (classes[character['class']] || '?') + ' / '
+		description += ' ' + (classes[character['class']] || '?')
 		return description
 	}
 })()
+//say = 0, party = 1, guild = 2, area = 3, trade = 4, greet = 9,
+//private = 11-18, p-notice = 21, emote = 26, global = 27, r-notice = 25,
+//raid = 32, megaphone = 213, guild-adv = 214
+chatChannels = {
+    0:'{#silver-fg}[Say]',
+    1:'{#48ADFF-fg}[Party]',
+    2:'{#12DE3A-fg}[Guild]',
+    3:'{#896BE3-fg}[Area]',
+    4:'{#BD9633-fg}[Trade]',
+    9:'{#FFBD00-fg}[Greeting]',
+    26:'{#FFC0CB-fg}',
+    27:'{yellow-fg}[Global]',
+    213:'{yellow-fg}[Megaphone]',
+    214:'{#6DBA6C-fg}[Guild-Ad]'
+}
+function parseTeraChat(evt){
+    msg = chatChannels[evt.channel]
+    if(screenshotmode) msg += '[Redacted.Name]: '
+    else msg += '['+evt.authorName+']: '
+    msg += npmstring(evt.message).stripTags().decodeHTMLEntities().s
+    return msg+"{/}"
+}
 
 const srv = servers[config.server]
 const web = new webClient(srv.srv, config.email, config.pass)
@@ -44,7 +208,7 @@ web.getLogin((err, data) => {
 	function closeClient() {
 		if (closed) return
 		closed = true
-        console.log("Shutting down Elixir...")
+        console.log("Shutting down TERA-CLI...")
 		client.close()
 		setImmediate(() => {
             console.log("Exiting")
@@ -73,20 +237,30 @@ web.getLogin((err, data) => {
 		dispatch.hook('S_GET_USER_LIST', 5, (event) => {
 			const characters = new Map()
 			for (const character of event.characters) {
-				characters.set(character.name.toLowerCase(), {
-					id: character.id,
-					description: `${character.name} [${describe(character)}]`
-				})
+                if(screenshotmode){
+                    characters.set(character.name.toLowerCase(), {
+                        id: character.id,
+                        description: `Redacted.Name [${describe(character)}]`
+                    })
+                }
+                else{
+                    characters.set(character.name.toLowerCase(), {
+                        id: character.id,
+                        description: `${character.name} [${describe(character)}]`
+                    })
+                }
 			}
-            console.log("Characters:")
+            content.pushLine("Characters:")
             for (const char of characters.values()) {
-                console.log(`- ${char.description} (id: ${char.id})`)
+                if(screenshotmode) content.pushLine(`> ${char.description} (id: redacted_id)`)
+                else content.pushLine(`> ${char.description} (id: redacted_id)`)
             }
 			const character = characters.get(config.character.toLowerCase())
 			if (!character) {
-				console.error(`[client] no character "${config.character}"`)
+				content.pushLine(`[client] no character "${config.character}"`)
 			} else {
-				console.log(`[client] logging onto ${character.description} (id: ${character.id})`)
+                if(screenshotmode) content.pushLine(`[client] logging onto ${character.description} (id: redacted_id)`)
+				else content.pushLine(`[client] logging onto ${character.description} (id: ${character.id})`)
 				dispatch.toServer('C_SELECT_USER', 1, {
 					id: character.id,
 					unk: 0
@@ -106,7 +280,9 @@ web.getLogin((err, data) => {
 				id : event.id
 			})
 		})
-
+        dispatch.hook('S_CHAT', 1, (event) => {
+			content.pushLine(parseTeraChat(event))
+		})
 		client.on('close', () => {
 			closeClient()
 		})
@@ -115,24 +291,25 @@ web.getLogin((err, data) => {
 	fs.readdirSync('./modules/').forEach(file => {
 		connection.dispatch.load('./modules/' + file, module)
 	})*/
-    connection.dispatch.load('./modules/teraCLI', module, closeClient)
+    //connection.dispatch.load('./modules/teraCLI', module, closeClient)
 	srvConn.setTimeout(10 * 1000)
 
 	srvConn.on('connect', () => {
-		console.log(`<connected to ${srvConn.remoteAddress}:${srvConn.remotePort}>`)
+        if(screenshotmode) content.pushLine(`Connected to <redacted_ip:redacted_port> aka Celestial Mount Tempest Ascension Forest`)
+		else content.pushLine(`Connected to <${srvConn.remoteAddress}:${srvConn.remotePort}> aka ${config.server}`)
 	})
 
 	srvConn.on('timeout', () => {
-		console.log('<timeout>')
+        content.pushLine('<timeout>')
 		closeClient()
 	})
 
 	srvConn.on('close', () => {
-		console.log('<disconnected>')
+		content.pushLine('<disconnected>')
 		process.exit()
 	})
 
 	srvConn.on('error', (err) => {
-		console.warn(err)
+		content.pushLine(err)
 	})
 })
